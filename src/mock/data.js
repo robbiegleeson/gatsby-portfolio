8c104977611634d4500d6adcc96f4d837bb07333
Rob Gleeson <robbie.gleeson@gmail.com>
2020-09-30 10:52:37 +0100

    [UPDATE] Update profile

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpeg',
+  img: 'rob-profile.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
